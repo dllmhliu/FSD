f4a6391f74f81dcffeff4cc2c63379bcab80c6c0
dllmhliu <dllmhliu@cn.ibm.com>
2019-07-24 13:22:26 +0800

    fix3

--- assignment3/src/AddVideo.js
+++ assignment3/src/AddVideo.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'whatwg-fetch';
 import 'es6-promise';
-
 class AddVideo extends React.Component {
 
     constructor(props) {
@@ -54,7 +53,8 @@ class AddVideo extends React.Component {
 
     addVideo() {
         let count = this.state.playlist.length;
-        let postData = {"id": count+1, "title": this.state.title, "url": this.state.url, "approved": false};
+       
+        let postData = {"id": Date.now(), "title": this.state.title, "url": this.state.url, "approved": false};
         var result = fetch('http://localhost:3000/playlist/',{
             method:'POST',
             headers:{
@@ -105,13 +105,72 @@ class AddVideo extends React.Component {
                 }
             });
           });
+          window.location.reload(true);
     }
+    playlistEdit(play) {
+        //let count = this.state.playlist.length;
+        //let postData = {"id": play.id, "title": this.state.title, "url": this.state.url, "approved": false};
+        // this.state.title.value=play.title;
+        // this.state.url=play.url;
+        // this.state.add="Save";
 
+        //let count = this.state.playlist.length;
+        //let postData = {"id": play.id, "title": this.state.title, "url": this.state.url, "approved": false};
+
+        play.title = this.state.title;
+        play.url = this.state.url;
+
+        var result = fetch('http://localhost:3000/playlist/'+play.id,{
+            method:'PUT',
+            headers:{
+              'Content-Type':'application/json;charset=UTF-8'
+            },
+            mode:'cors',
+            cache:'default',
+            body: JSON.stringify(play)
+          });
+          result.then(res => {
+              res.json().then(data => {
+                console.log("data:", data);
+                if (data != null) {
+                    this.getPlaylist();
+                }
+            });
+          });
+          window.location.reload(true);
+        // console.log(this.state.url);
+
+    }
+    playlistApprove(play) {
+        //let postData = {"id": play.id, "title": this.state.title, "url": this.state.url, "approved": true};
+        play.approved = true;
+        var result = fetch('http://localhost:3000/playlist/'+play.id,{
+            method:'PUT',
+            headers:{
+              'Content-Type':'application/json;charset=UTF-8'
+            },
+            mode:'cors',
+            cache:'default',
+            body: JSON.stringify(play)
+          });
+          result.then(res => {
+              res.json().then(data => {
+                console.log("data:", data);
+                if (data != null) {
+                    this.getPlaylist();
+                }
+            });
+          });
+          window.location.reload(true);
+        //   console.log("http://localhost:5000/");
+        //   window.location.reload
+    }
     render() {
         var playlistHtml = [];
+        let i=1;
         for (let play of this.state.playlist) {
-            playlistHtml.push(<tr key={play.id}><td>{play.id}&nbsp;&nbsp;</td><td>{play.title}&nbsp;&nbsp;</td><td>{play.url}&nbsp;&nbsp;</td><td><a href="javascript:void(0)" onClick={()=>this.playlistDelete(play)} style={{color:'blueviolet'}}>Edit</a>&nbsp;&nbsp; </td><td><a href="javascript:void(0)" onClick={()=>this.playlistDelete(play)} style={{color:'blueviolet'}}>Delete</a>&nbsp;&nbsp;</td><td> <a href="javascript:void(0)" onClick={()=>this.playlistDelete(play)} style={{color:'blueviolet'}}>Approve</a></td></tr>);
-
+            playlistHtml.push(<tr key={play.id}><td>{i}&nbsp;&nbsp;</td><td>{play.title}&nbsp;&nbsp;</td><td>{play.url}&nbsp;&nbsp;</td><td><a href="javascript:void(0)" onClick={()=>this.playlistEdit(play)} style={{color:'blueviolet'}}>Edit</a>&nbsp;&nbsp; </td><td><a href="javascript:void(0)" onClick={()=>this.playlistDelete(play)} style={{color:'blueviolet'}}>Delete</a>&nbsp;&nbsp;</td><td> <a id="approve" href="javascript:void(0)" onClick={()=>this.playlistApprove(play)} style={{color:'blueviolet'}}>Approve</a></td></tr>);
+            i++;
         }
         return (
             <div>
@@ -120,7 +179,7 @@ class AddVideo extends React.Component {
                 <input type="input" id="title" onChange={this.inputChangeHandler.bind(this)} defaultValue={this.state.title}/>
                 <label>URL:</label>
                 <input type="input" id="url" onChange={this.inputChangeHandler.bind(this)} defaultValue={this.state.url}/>&nbsp;&nbsp;
-                <input type="submit" value="Add Video" onClick={()=>{this.addVideo()}} />
+                <input id="add" type="submit" value="Add Video" onClick={()=>{this.addVideo()}} />
                 <table class="table table-hover">
                     <thead>
                         <tr>
--- assignment3/src/App.js
+++ assignment3/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import VideoPlayer from './VideoPlayer';
 function App() {
