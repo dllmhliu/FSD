9822e44ecc61f4e27f1f8e0e2fcf660cf04d151d
dllmhliu <dllmhliu@cn.ibm.com>
2019-10-06 19:47:56 +0800

    assignment5&6

--- assignment5/src/main/resources/db/schema-h2.sql
+++
@@ -1,5 +0,0 @@
-insert into user_info(id,name,password,email,role) values(1,'Ada','123456','ada@cn.ibm.com','admin');
-insert into user_info(id,name,password,email,role) values(2,'sky','123456','sky@cn.ibm.com','guest');
-
---insert into user_info(name,password,email,role) values('Ada','123456','ada@cn.ibm.com','admin');
---insert into user_info(name,password,email,role) values('sky','123456','sky@cn.ibm.com','guest');
